719215ec31fcff369a802dc912a53d6977625f9e
FirstBroRussia <chipanrussia@gmail.com>
2021-11-30 13:20:50 +0300

    Исправления

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,22 +1,11 @@
 const getRandomNumber = (min, max, fixed) => {
   if (fixed) {
     const randomFractionalNumber = (Math.random() * (max + 1 - min) + min).toFixed(1);
-    return randomFractionalNumber >= 10 ? '10' : randomFractionalNumber;
+    return randomFractionalNumber >= max ? max : randomFractionalNumber;
   }
   return Math.floor(Math.random() * (max + 1 - min) + min);
 };
 
 const getConvertedString = (string, specifiedLength) => string.length > specifiedLength ? `${string.slice(0, specifiedLength - 1)}...` : string;
 
-
-function switchForRenderHtmlElement (container, position, element) {
-  switch (position) {
-    case position.BEFORE_BEGIN: return container.before(element);
-    case position.AFTER_BEGIN: return container.prepend(element);
-    case position.BEFORE_END: return container.append(element);
-    case position.AFTER_END: return container.after(element);
-  }
-}
-
-
-export {getRandomNumber, getConvertedString, switchForRenderHtmlElement};
+export {getRandomNumber, getConvertedString};
